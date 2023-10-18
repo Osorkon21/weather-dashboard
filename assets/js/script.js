56cfa650b6c4c69e9031f1867583323bb7e02f11
Osorkon21 <Osorkon21@users.noreply.github.com>
2023-10-17 22:26:06 -0500

    Add comments, tweak HTML and JS a bit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// add comments
+// README
+// submit
+
 const body = $("body");
 const form = $("form");
 const cityNameInput = $("#city-name-input");
@@ -10,26 +14,31 @@ const currentHumidity = $(".current-humidity");
 const fiveDayForecastHeadline = $(".five-day-forecast-headline");
 const fiveDayForecastContainer = $(".five-day-forecast-container");
 
+// loads city search history
 var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
 start();
 
+// add buttons for city names loaded from search history
 function start() {
   if (searchHistory.length !== 0)
     for (var i = 0; i < searchHistory.length; i++)
       genSearchHistoryBtn(searchHistory[i]);
 }
 
+// Search button event handler
 function onCityNameInput(e) {
   e.preventDefault();
   var cityName = formatCityName(cityNameInput.val());
   displayWeather(cityName);
 }
 
+// reloads weather info
 function onSavedCityClick() {
   displayWeather($(this).text(), false);
 }
 
+// capitalizes first letter, lowercases the rest, ignores extra spaces
 function formatCityName(cityName) {
   var trimmedName = cityName.trim();
   var splitName = trimmedName.split(" ");
@@ -39,29 +48,33 @@ function formatCityName(cityName) {
     var firstLetter = splitName[i][0];
     var restOfWord = splitName[i].slice(1);
 
+    // check for empty strings
     if (firstLetter === undefined)
       continue;
 
     var word = (firstLetter.toUpperCase() + restOfWord.toLowerCase());
     capitalizedName += word;
 
+    // don't add an extra space at the end of the word
     if (i !== splitName.length - 1)
       capitalizedName += " ";
   }
 
-  // edge case
+  // handle edge case
   if (capitalizedName === "Washington D.c.")
     capitalizedName = "Washington D.C.";
 
   return capitalizedName;
 }
 
+// API query function, displays resulting API data
 async function displayWeather(cityName, saveHistory = true) {
   const currentWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${convertSpaces(cityName)}&appid=fe0219a594b50b6a018a708bc7c62289&units=imperial`;
 
   const currentWeatherResponse = await fetch(currentWeatherAPI);
   const currentWeatherData = await currentWeatherResponse.json();
 
+  // don't process bad data
   if (currentWeatherData.cod === "404" || currentWeatherData.cod === "400") {
     changeCityNameInput(true);
     return;
@@ -69,6 +82,7 @@ async function displayWeather(cityName, saveHistory = true) {
   else {
     changeCityNameInput(false);
 
+    // only save if city name is new
     if (saveHistory && !isNameAlreadySaved(cityName)) {
       saveSearchHistory(cityName);
       genSearchHistoryBtn(cityName);
@@ -77,7 +91,10 @@ async function displayWeather(cityName, saveHistory = true) {
 
   displayCurrentWeather(currentWeatherData);
 
+  // latitude
   const lat = currentWeatherData.coord.lat;
+
+  // longitude
   const lon = currentWeatherData.coord.lon;
 
   const cityAPI = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=fe0219a594b50b6a018a708bc7c62289&units=imperial`;
@@ -90,6 +107,7 @@ async function displayWeather(cityName, saveHistory = true) {
   displayFiveDayForecast(cityForecastData);
 }
 
+// replaces all spaces in URL queries with plus signs
 function convertSpaces(cityName) {
   for (var i = 0; i < cityName.length; i++)
     if (cityName[i] === " ")
@@ -98,9 +116,12 @@ function convertSpaces(cityName) {
   return cityName;
 }
 
+// resets search box contents
 function changeCityNameInput(cityNotFound) {
   if (cityNotFound) {
     cityNameInput.val("");
+
+    // error message if bad city name passed
     cityNameInput.attr("placeholder", "City not found! Try again.");
   }
   else {
@@ -109,6 +130,7 @@ function changeCityNameInput(cityNotFound) {
   }
 }
 
+// did user already search for this city
 function isNameAlreadySaved(cityName) {
   for (var i = 0; i < searchHistory.length; i++)
     if (cityName === searchHistory[i])
@@ -117,25 +139,38 @@ function isNameAlreadySaved(cityName) {
   return false;
 }
 
+// populates top right box with current weather conditions
 function displayCurrentWeather(currentWeatherData) {
+
+  // image icon name
   const imgIcon = currentWeatherData.weather[0].icon;
+
   const newImg = $("<img>").attr("src", `https://openweathermap.org/img/wn/${imgIcon}.png`);
 
+  // adds border around current weather conditions box
   todayForecast.attr("class", "today-forecast border border-white border-2 p-2");
 
+  // adds date
   boldTodayForecast.text(currentWeatherData.name + dayjs().format(" (M/D/YYYY)"));
   boldTodayForecast.append(newImg);
 
+  // adds current weather
   currentTemp.text(`Temp: ${Math.round(currentWeatherData.main.temp)}\xB0F`);
   currentWind.text(`Wind: ${Math.round(currentWeatherData.wind.speed)} MPH`);
   currentHumidity.text(`Humidity: ${currentWeatherData.main.humidity}%`);
 }
 
+// populates bottom right boxes with forecasted weather conditions
 function displayFiveDayForecast(cityForecastData) {
   fiveDayForecastHeadline.text("5-Day Forecast:");
+
+  // deletes any previous forecast boxes
   fiveDayForecastContainer.empty();
 
+  // build 5 boxes
   for (var i = 0; i < 5; i++) {
+
+    // gather weather conditions
     const weather = cityForecastData.list[8 * i];
     const date = dayjs(weather.dt_txt).format("M/D/YYYY");
     const imgIcon = weather.weather[0].icon;
@@ -143,6 +178,7 @@ function displayFiveDayForecast(cityForecastData) {
     const wind = `Wind: ${Math.round(weather.wind.speed)} MPH`;
     const humidity = `Humidity: ${weather.main.humidity}%`;
 
+    // add box as raw HTML
     fiveDayForecastContainer.append($(`
       <div class="border border-white border-2 p-2">
         <p class="h3">${date}</p>
@@ -155,17 +191,21 @@ function displayFiveDayForecast(cityForecastData) {
   }
 }
 
+// saves search history in localStorage
 function saveSearchHistory(cityName) {
   searchHistory.push(cityName);
   localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 }
 
+// adds searched-for city name to a new button
 function genSearchHistoryBtn(cityName) {
   savedCityBtns.append(`
   <button type="button" class="saved-city btn-outline-secondary bg-gradient">${cityName}</button>`);
 
+  // adds border below Search button
   form.attr("class", "d-grid border-bottom border-white border-2");
 }
 
+// event handlers for buttons
 body.on("click", ".search-btn", onCityNameInput);
 body.on("click", ".saved-city", onSavedCityClick);
